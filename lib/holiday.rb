c2d0e247ccf452032c8c2527ed0e13925aa36340
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-17 04:17:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,8 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
       end
-    end.flatten.compact
+    end
+    .flatten.compact
 end
 
 
